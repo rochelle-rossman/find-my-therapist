8c5a6e2bfec28a5d5fc843e5e1b3ef3fd1e3cfd3
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2022-09-04 11:15:37 -0500

    MVP complete

--- pages/user/[firebaseKey].js
+++ pages/user/[firebaseKey].js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { getSingleUser } from '../../api/userData';
+import { useAuth } from '../../utils/context/authContext';
 
 export default function ViewUser() {
+  const { user } = useAuth();
   const [userDetails, setUserDetails] = useState({});
   const router = useRouter();
 
@@ -13,8 +17,28 @@ export default function ViewUser() {
   }, [firebaseKey]);
   return (
     <>
-      <div>{userDetails.name}</div>
-      <div>{userDetails.gender}</div>
+      <Card style={{ width: '30rem' }}>
+        <Card.Img variant="top" src={userDetails?.photo} />
+        <Card.Body>
+          <Card.Title>{userDetails?.name}</Card.Title>
+          <Card.Text>{userDetails?.email}</Card.Text>
+          <Card.Text>{userDetails?.phone}</Card.Text>
+          <Card.Text>{userDetails?.pronouns}</Card.Text>
+          <Card.Text>{userDetails.bio}</Card.Text>
+        </Card.Body>
+        <Card.Body>
+          <Link href={`/user/${userDetails.firebaseKey}`} passHref>
+            <Button variant="primary">VIEW</Button>
+          </Link>
+          <Button variant="primary">MESSAGE</Button>
+          <Button variant="primary">SAVE</Button>
+          <Link href={`/user/edit/${userDetails.firebaseKey}`} passHref>
+            <Button variant="success" className={userDetails.uid !== user.uid ? 'noShow' : ''}>
+              EDIT
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
     </>
   );
 }
